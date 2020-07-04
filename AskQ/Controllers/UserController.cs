88a01fa5a9e96d6298575533ca4505d89e4dcf02
Youssef Victor <youssefvictor00@gmail.com>
2020-07-04 12:36:29 +0200

    Add initial UserProfile page

---
+++ AskQ/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using AskQ.Data;
+using AskQ.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AskQ.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET /username
+        [HttpGet("{username}")]
+        public IActionResult UserProfile(string username, int page = 1)
+        {
+            IdentityUser user = _dbContext.Users.FirstOrDefault(u => u.UserName == username);
+            if (user is null)
+            {
+                return NotFound();
+            }
+            var viewModel = new UserProfileViewModel
+            {
+                PageNumber = page,
+                User = user,
+                // TODO: Skip depending on page.
+                //Questions = _dbContext.Questions.Include(q => q.Replies).Include(q => q.AskedTo).Where(q => q.AskedTo.UserName == username).Take(10).AsEnumerable()
+            };
+            return View(viewModel);
+        }
+
+       /* [HttpPost]
+        public IActionResult PostQuestion(string username, string question)
+        {
+
+        }*/
+    }
+}
---
+++ AskQ/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using AskQ.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace AskQ.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public int PageNumber { get; set; }
+        public IdentityUser User { get; set; } = null!;
+        public IEnumerable<Question>? Questions { get; set; }
+    }
+}
