13790e1537908ccad9e61e29fe240e6cf1f9e719
Youssef Victor <youssefvictor00@gmail.com>
2020-07-04 16:25:52 +0200

    Separate questions into own controller

---
+++ AskQ/Controllers/QuestionsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AskQ.Data;
+using AskQ.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AskQ.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public QuestionsController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(string username, string questionText, string receivingId)
+        {
+            if (string.IsNullOrWhiteSpace(questionText))
+            {
+                return BadRequest();
+            }
+
+            IdentityUser? askedTo = await _dbContext.Users.FindAsync(receivingId);
+            if (askedTo is null)
+            {
+                return NotFound();
+            }
+
+            IdentityUser? askedFrom = null;
+            if (User.Identity.IsAuthenticated)
+            {
+                askedFrom = await _dbContext.Users.FirstAsync(u => u.UserName == User.Identity.Name);
+            }
+
+            var newQuestion = new Question
+            {
+                AskedTo = askedTo,
+                AskedFrom = askedFrom,
+                QuestionText = questionText,
+                DateTime = DateTime.Now,
+                Replies = Enumerable.Empty<Reply>()
+            };
+            await _dbContext.Questions.AddAsync(newQuestion);
+            await _dbContext.SaveChangesAsync();
+            TempData["Message"] = "Your question has been sent. Questions appear only after they are answered.";
+            return RedirectToAction("UserProfile", "User", new { username = askedTo.UserName });
+        }
+
+        [HttpGet]
+        public IActionResult Received()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return BadRequest();
+            }
+            return View(_dbContext.Questions.Include(q => q.AskedTo).Where(q => q.AskedTo.UserName == User.Identity.Name && !q.Replies.Any()).AsEnumerable());
+        }
+    }
+}
--- AskQ/Controllers/UserController.cs
+++ AskQ/Controllers/UserController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using AskQ.Data;
-using AskQ.Models;
 using AskQ.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -35,51 +33,6 @@ namespace AskQ.Controllers
                 Questions = _dbContext.Questions.Include(q => q.Replies).Include(q => q.AskedTo).Where(q => q.AskedTo.UserName == username && q.Replies.Any()).Skip(10 * (page - 1)).Take(10).AsEnumerable()
             };
             return View(viewModel);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> PostQuestionAsync(string username, string questionText, string receivingId)
-        {
-            if (string.IsNullOrWhiteSpace(questionText))
-            {
-                return BadRequest();
-            }
-
-            IdentityUser? askedTo = await _dbContext.Users.FindAsync(receivingId);
-            if (askedTo is null)
-            {
-                return NotFound();
-            }
-
-            IdentityUser? askedFrom = null;
-            if (User.Identity.IsAuthenticated)
-            {
-                askedFrom = await _dbContext.Users.FirstAsync(u => u.UserName == User.Identity.Name);
-            }
-
-            var newQuestion = new Question
-            {
-                AskedTo = askedTo,
-                AskedFrom = askedFrom,
-                QuestionText = questionText,
-                DateTime = DateTime.Now,
-                Replies = Enumerable.Empty<Reply>()
-            };
-            await _dbContext.Questions.AddAsync(newQuestion);
-            await _dbContext.SaveChangesAsync();
-            TempData["Message"] = "Your question has been sent. Questions appear only after they are answered.";
-            return RedirectToAction(nameof(UserProfileAsync), new { username = askedTo.UserName });
-        }
-
-        // The pattern here is intentional. I want the URL to look like /MyQuestions instead of /User/MyQuestions
-        [HttpGet(nameof(MyQuestions))]
-        public IActionResult MyQuestions()
-        {
-            if (!User.Identity.IsAuthenticated)
-            {
-                return BadRequest();
-            }
-            return View(_dbContext.Questions.Include(q => q.AskedTo).Where(q => q.AskedTo.UserName == User.Identity.Name && !q.Replies.Any()).AsEnumerable());
         }
     }
 }
