7e3f2a8781df11416dddc94f1053eba903687fc9
Youssef Victor <youssefvictor00@gmail.com>
2020-07-22 15:58:47 +0200

    Use Compound Assignment

--- AskQ/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ AskQ/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -62,7 +62,7 @@ namespace AskQ.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl ??= Url.Content("~/");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
@@ -74,7 +74,7 @@ namespace AskQ.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl ??= Url.Content("~/");
 
             if (ModelState.IsValid)
             {
