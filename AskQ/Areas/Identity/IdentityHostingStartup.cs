0a427b38fc75c1ba5128aece4de707d4c0711d01
Youssef Victor <youssefvictor00@gmail.com>
2020-07-22 14:36:22 +0200

    Scaffold Login and Register pages

---
+++ AskQ/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AskQ.Infrastructure.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AskQ.Areas.Identity.IdentityHostingStartup))]
+namespace AskQ.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
