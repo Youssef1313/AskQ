deef1b5808eb9a9094f654552071e6bdb6fe8db2
Youssef Victor <youssefvictor00@gmail.com>
2020-07-22 15:38:58 +0200

    Fix formatting

--- AskQ/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ AskQ/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -22,7 +22,7 @@ namespace AskQ.Areas.Identity.Pages.Account
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<ApplicationUser> signInManager, 
+        public LoginModel(SignInManager<ApplicationUser> signInManager,
             ILogger<LoginModel> logger,
             UserManager<ApplicationUser> userManager)
         {
